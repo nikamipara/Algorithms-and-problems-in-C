127df75bdd642e5d33308ab54e39475ee0f00f96
Nikunj <nikamipara29@gmail.com>
2015-07-05 09:59:32 +0530

    compiled files

---
+++ Untitled2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+    char a;
+    int i;
+    char z='z';
+    z++;
+    z++;
+
+    a = (char)0;
+        for(i=0;i<500;a++,i++){
+            printf("%c\n",a);
+        }
+
+
+}
